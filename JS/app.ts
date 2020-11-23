91c59a64e48c81c31c1810473c67d9d034e2ef59
Aryan <aryan2563737@gmail.com>
2020-11-24 03:59:41 +0530

    Sections init

--- JS/app.ts
+++ JS/app.ts
@@ -3,8 +3,16 @@ const $ = (element) => {
 }
 
 const homeText = $(".home__text");
-
+const logo = $('.aboutIIChE__logo');
+const IIChE_Content = $('.aboutIIChE__content')
 window.addEventListener('scroll', e => {
     homeText.style.transform = `translateX(${pageYOffset / 2}px)`;
-    console.log(pageYOffset / 2)
+
+    if (pageYOffset < 2 * window.innerHeight || pageYOffset > 3 * window.innerHeight) {
+
+        let x = Math.abs(pageYOffset - window.innerHeight * 2);
+        console.log(x);
+        logo.style.transform = `translateX(${-x}px)`
+        IIChE_Content.style.transform = `translateX(${x}px)`
+    }
 })
